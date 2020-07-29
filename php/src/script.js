5797ba25b01b4fb1be36b0c1ff432ea33d7dceae
Jordan Ho <40032616+JordanHo2782@users.noreply.github.com>
2020-07-29 19:37:47 +0800

    Add files via upload

--- php/src/script.js
+++ php/src/script.js
@@ -1,1 +1,36 @@
 
+
+// dept_manager_data.addEventListener('mouseenter', (event)=>{
+//     let xPosition = event.pageX
+//     let yPosition = event.pageY
+//     console.log(`${xPosition},${yPosition}`)
+// })
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const dept_manager_data = document.getElementById('dept_manager_data')
+//     dept_manager_data.addEventListener('mouseover', (event)=>{
+//         var xPosition = event.pageX
+//         var yPosition = event.pageY
+//         console.log(event.target.innerHTML)
+//         console.log(`${xPosition},${yPosition}`)
+//     })
+//     dept_manager_data.addEventListener('mouseout', (event)=>{
+//         var xPosition = event.pageX
+//         var yPosition = event.pageY
+//         console.log(`${xPosition},${yPosition}`)
+//     })
+// });
+
+const showInfoBox=(event)=>{
+
+    const PopBoxID = event.target.children[0].innerHTML
+    const PopBox = document.getElementById(PopBoxID)
+    PopBox.className = 'Pop DisplayBlock'
+}
+
+const hideInfoBox=(event)=>{
+    const PopBoxID = event.target.children[0].innerHTML
+    const PopBox = document.getElementById(PopBoxID)
+    PopBox.className = 'Pop DisplayNone'
+
+}
